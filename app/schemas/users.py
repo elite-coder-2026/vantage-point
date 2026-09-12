0e8ab2b4754ad5e7c4a67b9fb5eb9558f4f0a806
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-09-11 20:11:11 -0500

    Add app/schemas/users.py

---
+++ app/schemas/users.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    display_name: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    display_name: str
+    bio: str | None
+    is_private: bool
+    created_at: datetime
+
+
+class UserUpdate(BaseModel):
+    display_name: str | None = None
+    bio: str | None = None
+    is_private: bool | None = None
