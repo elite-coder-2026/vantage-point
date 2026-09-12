3323cfc0e1ed90b29c39035c81314d631ecd33a3
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-09-12 14:33:49 -0500

    Mount media/avatar static dirs and wire up the notify router

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from app.config import settings as app_settings
 from app.db import create_pool
-from app.routers import auth, users, follows, groups, posts, hashtags, pages, settings
+from app.routers import auth, users, follows, groups, posts, hashtags, notify, pages, settings
 
 
 @asynccontextmanager
@@ -17,6 +18,16 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount(
+    app_settings.media_base_url,
+    StaticFiles(directory=app_settings.media_storage_path, check_dir=False),
+    name="media",
+)
+app.mount(
+    app_settings.avatar_base_url,
+    StaticFiles(directory=app_settings.avatar_storage_path, check_dir=False),
+    name="avatars",
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
@@ -24,5 +35,6 @@ app.include_router(follows.router)
 app.include_router(groups.router)
 app.include_router(posts.router)
 app.include_router(hashtags.router)
+app.include_router(notify.router)
 app.include_router(pages.router)
 app.include_router(settings.router)
